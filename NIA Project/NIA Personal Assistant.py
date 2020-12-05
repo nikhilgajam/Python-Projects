4965e9089d5d43076dae4ccc0d815ff16ecc87a9
Nikhil Gajam <65104684+nikhilgajam@users.noreply.github.com>
2020-12-05 16:52:49 +0530

    NIA Personal Assistant

--- NIA Project/NIA Personal Assistant.py
+++ NIA Project/NIA Personal Assistant.py
@@ -25,23 +25,32 @@ search_list = []
 search_count = 0
 check = None
 
+
 # Operations
 
 
 def ask_pressed(event=""):
+    display['state'] = NORMAL
     display.insert(END, "\n\n>>> " + str(ques.get()))
     display.yview(END)
     ask(str(ques.get()))
+    display['state'] = DISABLED
+    print(threading.active_count())
 
 
 def ask(string):
     global search_list, search_count, check
+    # Search record
     search_list.append(string)
+    # Cleaning the input string
     string = string.strip().lower()
+
+    display['state'] = NORMAL
+    ques.focus()
 
     if string.startswith("search"):
         string = string.replace("search", "")
-        threading.Thread(target=sounds, args=("sounds/web.mp3", )).start()
+        threading.Thread(target=sounds, args=("sounds/web.mp3",)).start()
         display.insert(END, "\n Redirecting your query to a web browser.")
         display.yview(END)
         webbrowser.open('https://www.google.com/search?q=' + string)
@@ -49,13 +58,13 @@ def ask(string):
     elif string.endswith('hello') or string.endswith('hello nia'):
         display.insert(END, "\n " + "Hey. How can I help you?")
 
-    elif string.endswith('hey') or string.endswith('hey nia'):
+    elif string.startswith('hey') or string.endswith('hey nia'):
         display.insert(END, "\n " + "Hello. How can I help you?")
 
-    elif string.endswith('hi') or string.endswith('hi nia'):
+    elif string.startswith('hi') or string.endswith('hi nia'):
         display.insert(END, "\n " + "Hello. How can I help you?")
 
-    elif string.startswith('nia') or string.endswith('nia'):
+    elif string.startswith('nia'):
         display.insert(END, "\n " + "NIA is a Personal Assistant Created By Nikhil")
 
     elif string.endswith('your name'):
@@ -68,14 +77,14 @@ def ask(string):
             del temp
             display.insert(END, "\n " + "Done.")
         except Exception:
-            threading.Thread(target=internet_search, args=(str(string), )).start()
-        
+            threading.Thread(target=internet_search, args=(str(string),)).start()
+
     elif string == '':
         display.insert(END, "\n " + "Try to type a question or name or anything.")
-        threading.Thread(target=sounds, args=("sounds/type.mp3", )).start()
+        threading.Thread(target=sounds, args=("sounds/type.mp3",)).start()
 
     elif string.startswith('date') or string.endswith('date'):
-        threading.Thread(target=sounds, args=("sounds/date.mp3", )).start()
+        threading.Thread(target=sounds, args=("sounds/date.mp3",)).start()
         t = time.ctime()
         t = t[:10] + "," + t[19:]
         display.insert(END, "\n " + str(t))
@@ -84,11 +93,11 @@ def ask(string):
         if check is None:
             display.insert(END, "\n There's a problem with internet connection. Please retry.")
             display.yview(END)
-            threading.Thread(target=sounds, args=("sounds/retry.mp3", )).start()
+            threading.Thread(target=sounds, args=("sounds/retry.mp3",)).start()
             threading.Thread(target=internet_check, args=()).start()
             return
 
-        threading.Thread(target=sounds, args=("sounds/searching.mp3", )).start()
+        threading.Thread(target=sounds, args=("sounds/searching.mp3",)).start()
         client = wolframalpha.Client("U4L9E5-8RHX97YV24")
         res = client.query(string)
         data = next(res.results).text
@@ -99,7 +108,7 @@ def ask(string):
         threading.Thread(target=internet_check, args=()).start()
 
     elif string.endswith('time') or string.endswith('time now'):
-        threading.Thread(target=sounds, args=("sounds/time.mp3", )).start()
+        threading.Thread(target=sounds, args=("sounds/time.mp3",)).start()
         t = time.ctime()
         change = int(t[11:13]) % 12
         temp = t[11:]
@@ -110,28 +119,26 @@ def ask(string):
     elif string == 'calendar':
         t = time.ctime()
         t = t[:10] + "," + t[19:]
-        months = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
+        months = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10,
+                  "Nov": 11, "Dec": 12}
         display.insert(END, "\n" + calendar.month(int(t[-4:]), int(months[t[4:7].strip()])))
         display.insert(END, "\n" + str(t))
         display.yview(END)
 
-    elif string.startswith('weather'):
-        string = string.split(" ")
-
-        threading.Thread(target=weather_data, args=(string, )).start()
-
+    elif 'weather' in string:
         if check is None:
             display.insert(END, "\n There's a problem with internet connection. Please retry.")
             display.yview(END)
-            threading.Thread(target=sounds, args=("sounds/retry.mp3", )).start()
+            threading.Thread(target=sounds, args=("sounds/retry.mp3",)).start()
             threading.Thread(target=internet_check, args=()).start()
             return
 
+        threading.Thread(target=weather_data, args=(string,)).start()
 
     elif string == 'help':
         display.insert(END, "\n" + " weather <city-name> - Displays weather data in a particular city\n"
                                    " search  <keyword>   - Redirects your query to a web browser\n"
-                                   " open <app or file>  - Opens a particular app or file\n"
+                                   " open <app or file>  - Opens a specific app/file or website\n"
                                    " calendar            - Displays this month's calendar\n"
                                    " date                - Displays today's date\n"
                                    " time                - Displays time\n\n"
@@ -139,61 +146,86 @@ def ask(string):
         display.yview(END)
 
     else:
+        display['state'] = DISABLED
         # This will search the wolfram alpha if not found then searches on wikipedia after that searches on google
-        threading.Thread(target=internet_search, args=(str(string), )).start()
+        threading.Thread(target=internet_search, args=(str(string),)).start()
 
     ques.delete(0, END)
     search_count = 0
+    display['state'] = DISABLED
 
 
 def internet_search(string):
     if check is None:
         display.insert(END, "\n There's a problem with internet connection. Please retry.")
         display.yview(END)
-        threading.Thread(target=sounds, args=("sounds/retry.mp3", )).start()
+        threading.Thread(target=sounds, args=("sounds/retry.mp3",)).start()
         threading.Thread(target=internet_check, args=()).start()
         return
+
     try:
-        threading.Thread(target=sounds, args=("sounds/searching.mp3", )).start()
+        # Searches on wolfram alpha
+        threading.Thread(target=sounds, args=("sounds/searching.mp3",)).start()
         client = wolframalpha.Client("U4L9E5-8RHX97YV24")
         res = client.query(string)
         data = next(res.results).text
         if data == '(no data available)' or data == '(data not available)':
+            # If the answer is not found an exception is raised
             raise Exception
+        display['state'] = NORMAL
         display.insert(END, "\n " + str(data))
         display.yview(END)
+        display['state'] = DISABLED
     except Exception:
         try:
+            # Then searches on wikipedia
             data = wikipedia.summary(string, sentences=3)
+            display['state'] = NORMAL
             display.insert(END, "\n " + str(data))
-            display.insert(END, "\n\nIf this answer is not acceptable try placing 'search' in front of the query.\nEx: search Nikhil Tech")
+            display.insert(END,
+                           "\n\nIf this answer is not acceptable try placing 'search' in front of the query.\nEx: search Nikhil Tech")
             display.yview(END)
+            display['state'] = DISABLED
         except Exception:
-            threading.Thread(target=sounds, args=("sounds/web.mp3", )).start()
+            # When answer is not found on wikipedia then it open a web browser and searches on google
+            threading.Thread(target=sounds, args=("sounds/web.mp3",)).start()
+            display['state'] = NORMAL
             display.insert(END, "\n Redirecting your query to a web browser.")
             display.yview(END)
+            display['state'] = DISABLED
             webbrowser.open('https://www.google.com/search?q=' + string)
 
     threading.Thread(target=internet_check, args=()).start()
 
 
 def weather_data(string):
-    # We can also do this with string.replace("weather in", "")
+    city = ""
+    display['state'] = NORMAL
 
-    temp = string
+    # Cleaning the string to get weather data or weather help
 
-    try:
-        city = string[string.index('in') + 1]
-        if len(string) - 1 > string.index('in') + 2:
-            city = string[string.index('in') + 1] + " " + string[string.index('in') + 2] + " " + string[string.index('in') + 3]
-        elif len(string) - 1 > string.index('in') + 1:
-            city = string[string.index('in') + 1] + " " + string[string.index('in') + 2]
-    except Exception:
-        display.insert(END, "\n " + "How To Get Weather Data: \nEx: Weather in city (or) What's the weather in hyderabad")
+    if string.startswith('weather in '):
+        city = string.replace("weather in ", "")
+    elif string.startswith("what's the weather"):
+        city = string.replace("what's the weather in ", "")
+    elif string.startswith("whats the weather"):
+        city = string.replace("whats the weather in ", "")
+    elif string.startswith("how's the weather"):
+        city = string.replace("how's the weather in ", "")
+    elif string.startswith("hows the weather"):
+        city = string.replace("hows the weather in ", "")
+    elif string.startswith("weather help"):
+        display.insert(END,
+                       "\n " + "How To Get Weather Data: \nEx: Weather in city (or) What's the weather in hyderabad (or) How's the weather in hyderabad")
         display.yview(END)
         return
+
+    city = city.strip()
+
     try:
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=0c42f7f6b53b244c78a418f4f181282a&units=metric'.format(city)
+        # Weather data program
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=0c42f7f6b53b244c78a418f4f181282a&units=metric'.format(
+            city)
         dat = requests.get(url)
         data = dat.json()
         temp = data['main']['temp']
@@ -207,12 +239,15 @@ def weather_data(string):
         latitude = data['coord']['lat']
         longitude = data['coord']['lon']
         description = data['weather'][0]['description']
-        info = "City : {}\nCountry : {}\nTemperature : {}°C\nDescription : {}\nMinimum Temperature : {}°C\nMaximum Temperature : {}°C\nHumidity : {}%\nPressure : {} hpa\nWind Speed : {} m/s\nLatitude : {}\nLongitude : {}".format(place, country, temp, description, min_temp, max_temp, humidity, pressure, wind_speed, latitude, longitude)
-        threading.Thread(target=sounds, args=("sounds/weather.mp3", )).start()
+        info = "City : {}\nCountry : {}\nTemperature : {}°C\nDescription : {}\nMinimum Temperature : {}°C\nMaximum Temperature : {}°C\nHumidity : {}%\nPressure : {} hpa\nWind Speed : {} m/s\nLatitude : {}\nLongitude : {}".format(
+            place, country, temp, description, min_temp, max_temp, humidity, pressure, wind_speed, latitude, longitude)
+        threading.Thread(target=sounds, args=("sounds/weather.mp3",)).start()
         display.insert(END, "\n " + str(info))
         display.yview(END)
+
     except Exception:
-        threading.Thread(target=internet_search, args=(temp, )).start()
+        # When weather data is not found then it searches on internet
+        threading.Thread(target=internet_search, args=(string,)).start()
 
 
 def internet_check():
@@ -226,6 +261,9 @@ def internet_check():
 
 def search_track(event=""):
     global search_list, search_count
+
+    # When up arrow is pressed then this function is triggered and previous search strings are displayed
+
     try:
         ques.delete(0, END)
         search_count = search_count - 1
@@ -239,6 +277,9 @@ def sounds(source):
 
 
 def copy():
+    display.tag_add(SEL, "1.0", END)
+    display.mark_set(INSERT, 1.0)
+    display.see(INSERT)
     display.clipboard_clear()
     display.clipboard_append(display.selection_get())
 
@@ -249,12 +290,19 @@ def select_all():
     display.see(INSERT)
 
 
+def clicked(event=""):
+    ques.focus()
+
+
 def helps():
-    messagebox.showinfo("Help", "You Can Scroll The Display\nCheck internet error also refers to keyword error")
+    ques.insert(0, "help")
+    ask_pressed()
+    messagebox.showinfo("Help", "You Can Scroll The Display")
 
 
 def about():
     messagebox.showinfo("About", "NIA is a personal assistant created by Nikhil")
+    webbrowser.open("https://youtube.com/NikhilTech")
 
 
 def popup(event):
@@ -274,8 +322,9 @@ types = Font(family="Lucida Console")
 
 # Data Display
 
-display = Text(root, font=types, foreground="#dadce0", background="#35363a", undo=True, insertbackground="#dadce0", wrap="word")
+display = Text(root, font=types, foreground="#dadce0", background="#35363a", undo=True, insertbackground="#dadce0", wrap="word", cursor="arrow")
 display.bind("<Button-3>", popup)  # Popup Menu Binding
+display.bind_all("<Button-1>", clicked)
 display.pack(fill=BOTH, expand=True)
 
 box = Frame(root)
@@ -297,7 +346,8 @@ ask_btn.grid(row=0, column=1, padx=3, pady=6)
 # Welcome string with sound
 
 display.insert(END, ">>> Hey there. I'm NIA (Nikhil's Assistant). How can I help you?")
-threading.Thread(target=sounds, args=("sounds/greeting.mp3", )).start()
+threading.Thread(target=sounds, args=("sounds/greeting.mp3",)).start()
+display['state'] = DISABLED
 
 # Internet Check
 
@@ -307,12 +357,7 @@ threading.Thread(target=internet_check, args=()).start()
 
 popup_menu = Menu(root, tearoff=0)
 
-popup_menu.add_command(label="Copy", command=copy)
-popup_menu.add_separator()
-popup_menu.add_command(label="Undo", command=display.edit_undo)
-popup_menu.add_command(label="Redo", command=display.edit_redo)
-popup_menu.add_separator()
-popup_menu.add_command(label="Select All\t\t\t", command=select_all)
+popup_menu.add_command(label="Copy Data", command=copy)
 popup_menu.add_separator()
 popup_menu.add_command(label="Help", command=helps)
 popup_menu.add_separator()

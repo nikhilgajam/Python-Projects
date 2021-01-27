95f666583198500fbe16130a08879fb0ac86cc7e
Nikhil Gajam <65104684+nikhilgajam@users.noreply.github.com>
2021-01-27 08:54:22 +0530

    Updated the program

--- Matrix Calculations Project/Matrix Calculations.py
+++ Matrix Calculations Project/Matrix Calculations.py
@@ -10,9 +10,14 @@ root.geometry("883x652")
 root.iconbitmap(True, 'images\\icon.ico')  # Smooth Icon Management
 root.resizable(0, 0)
 
+# data Stores the heading text
 data = StringVar()
-count = 0
 
+# mat1&2_data stores array values
+mat1_data = ""
+mat2_data = ""
+
+# Heading
 label = Label(root, textvariable=data, font="Pristina 25")
 label.pack(expand=TRUE, fill=BOTH)
 data.set("Matrix Calculations")
@@ -20,295 +25,213 @@ data.set("Matrix Calculations")
 # Commands
 
 
-def add():
+def get_data(num):
     try:
-        r1 = int(er1.get())
-        c1 = int(ec1.get())
-        r2 = int(er2.get())
-        c2 = int(ec2.get())
+        # Row and column data
+        # if the num value is 1 then it indicates both rows and columns need to be entered else one row and one column is enough
 
-        data.set("Matrix Addition")
-        arr = []
-        arr = (e1.get())
+        if num == 1:
+            r1 = int(er1.get().strip())
+            c1 = int(ec1.get().strip())
+            r2 = int(er2.get().strip())
+            c2 = int(ec2.get().strip())
+        else:
+            r1 = int(er1.get().strip())
+            c1 = int(ec1.get().strip())
+            r2 = 0
+            c2 = 0
+
+        # Changing the text data to list by splitting
+
+        arr = str(mat1_data).strip().replace("\n", " ")
         arr = arr.split(" ")
-        k = 0
-        for i in arr:
-            arr[k] = float(i)
-            k += 1
 
+        if num == 1:
+            arr1 = str(mat2_data).strip().replace("\n", " ")
+            arr1 = arr1.split(" ")
+        else:
+            arr1 = 0
+
+        # Changing the list data to float data type
+        for i, k in enumerate(arr):
+            arr[i] = float(k)
+
+        if num == 1:
+            for i, k in enumerate(arr1):
+                arr1[i] = float(k)
+
+        # Converting the list to Numpy array
         arr = np.array([arr])   # We can add dtype = int64 and more
-
-        arr1 = []
-        arr1 = (e2.get())
-        arr1 = arr1.split(" ")
-
-        k = 0
-        for j in arr1:
-            arr1[k] = float(j)
-            k += 1
-
         arr1 = np.array([arr1])
-        ans = 0
 
         try:
+            # Changing the linear array to the array that we entered in row and column boxes
             arr = np.reshape(arr, (r1, c1))
-            arr1 = np.reshape(arr1, (r2, c2))
-            ans = arr+arr1
+            if num == 1:
+                arr1 = np.reshape(arr1, (r2, c2))
 
-            text.delete(1.0, END)
-            text.insert(END, "Entered Matrix 1:\n" + str(arr)+"\n"+"Entered Matrix 2:" + "\n" + str(arr1))
-            text.insert(END, "\n\nAnswer:\n"+str(ans))
+            return arr, arr1
 
         except ValueError:
             text.delete(1.0, END)
             tm.showerror("Dimensional Error", "Check The Entered Values")
+            # When exception is triggered then it return None values
+            return None, None
 
     except ValueError:
         text.delete(1.0, END)
-        tm.showerror("Value Error", "Enter Valid Input(Only Numbers)")
+        tm.showerror("Value Error", "Enter Valid Input(Only Numbers)\nAnd Don't Leave Row And Column Boxes Blank")
+        # When exception is triggered then it return None values
+        return None, None
+
+
+def add():
+    data.set("Matrix Addition")
+
+    arr, arr1 = get_data(1)
+    if arr is None or arr1 is None:
+        return
+
+    # Array addition
+    ans = arr + arr1
+
+    text.delete(1.0, END)
+    text.insert(END, "Entered Matrix 1:\n" + str(arr)+"\n"+"Entered Matrix 2:" + "\n" + str(arr1))
+    text.insert(END, "\n\nAnswer:\n"+str(ans))
 
 
 def sub():
-    try:
-        r1 = int(er1.get())
-        c1 = int(ec1.get())
-        r2 = int(er2.get())
-        c2 = int(ec2.get())
+    data.set("Matrix Subtraction")
 
-        data.set("Matrix Subtraction")
-        arr = []
-        arr = (e1.get())
-        arr = arr.split(" ")
-        k = 0
-        for i in arr:
-            arr[k] = float(i)
-            k += 1
+    arr, arr1 = get_data(1)
+    if arr is None or arr1 is None:
+        return
 
-        arr = np.array([arr])   # We can add dtype = int64 and more
+    # Array subtraction
+    ans = arr - arr1
 
-        arr1 = []
-        arr1 = (e2.get())
-        arr1 = arr1.split(" ")
-
-        k = 0
-        for j in arr1:
-            arr1[k] = float(j)
-            k += 1
-
-        arr1 = np.array([arr1])
-        ans = 0
-
-        try:
-            arr = np.reshape(arr, (r1, c1))
-            arr1 = np.reshape(arr1, (r2, c2))
-            ans = arr-arr1
-
-            text.delete(1.0, END)
-            text.insert(END, "Entered Matrix 1:\n" + str(arr)+"\n" + "Entered Matrix 2:" + "\n" + str(arr1))
-            text.insert(END, "\n\nAnswer:\n"+str(ans))
-
-        except ValueError:
-            text.delete(1.0, END)
-            tm.showerror("Dimensional Error", "Check The Entered Values")
-
-    except ValueError:
-        text.delete(1.0, END)
-        tm.showerror("Value Error", "Enter Valid Input(Only Numbers & Decimals)")
+    text.delete(1.0, END)
+    text.insert(END, "Entered Matrix 1:\n" + str(arr)+"\n" + "Entered Matrix 2:" + "\n" + str(arr1))
+    text.insert(END, "\n\nAnswer:\n"+str(ans))
 
 
 def mult():
-    try:
-        r1 = int(er1.get())
-        c1 = int(ec1.get())
-        r2 = int(er2.get())
-        c2 = int(ec2.get())
+    data.set("Matrix Multiplication")
 
-        data.set("Matrix Multiplication")
-        arr = []
-        arr = (e1.get())
-        arr = arr.split(" ")
-        k = 0
-        for i in arr:
-            arr[k] = float(i)
-            k += 1
+    arr, arr1 = get_data(1)
+    if arr is None or arr1 is None:
+        return
 
-        arr = np.array([arr])   # We can add dtype = int64 and more
+    # Array multiplication
+    ans = arr.dot(arr1)
 
-        arr1 = []
-        arr1 = (e2.get())
-        arr1 = arr1.split(" ")
-
-        k = 0
-        for j in arr1:
-            arr1[k] = float(j)
-            k += 1
-
-        arr1 = np.array([arr1])
-        ans = 0
-
-        try:
-            arr = np.reshape(arr, (r1, c1))
-            arr1 = np.reshape(arr1, (r2, c2))
-            ans = arr.dot(arr1)
-
-            text.delete(1.0, END)
-            text.insert(END, "Entered Matrix 1:\n" + str(arr)+"\n" + "Entered Matrix 2:" + "\n" + str(arr1))
-            text.insert(END, "\n\nAnswer:\n"+str(ans))
-
-        except ValueError:
-            text.delete(1.0, END)
-            tm.showerror("Dimensional Error", "Check The Entered Values")
-
-    except ValueError:
-        text.delete(1.0, END)
-        tm.showerror("Value Error", "Enter Valid Input(Only Numbers & Decimals)")
+    text.delete(1.0, END)
+    text.insert(END, "Entered Matrix 1:\n" + str(arr)+"\n" + "Entered Matrix 2:" + "\n" + str(arr1))
+    text.insert(END, "\n\nAnswer:\n"+str(ans))
 
 
 def div():
-    try:
-        r1 = int(er1.get())
-        c1 = int(ec1.get())
-        r2 = int(er2.get())
-        c2 = int(ec2.get())
+    data.set("Matrix Division")
 
-        data.set("Matrix Division")
-        arr = []
-        arr = (e1.get())
-        arr = arr.split(" ")
-        k = 0
-        for i in arr:
-            arr[k] = float(i)
-            k += 1
+    arr, arr1 = get_data(1)
+    if arr is None or arr1 is None:
+        return
 
-        arr = np.array([arr])   # We can add dtype = int64 and more
+    # Array division
+    ans = arr / arr1
 
-        arr1 = []
-        arr1 = (e2.get())
-        arr1 = arr1.split(" ")
-
-        k = 0
-        for j in arr1:
-            arr1[k] = float(j)
-            k += 1
-
-        arr1 = np.array([arr1])
-        ans = 0
-
-        try:
-            arr = np.reshape(arr, (r1, c1))
-            arr1 = np.reshape(arr1, (r2, c2))
-            ans = arr/arr1
-
-            text.delete(1.0, END)
-            text.insert(END, "Entered Matrix 1:\n" + str(arr) + "\n" + "Entered Matrix 2:" + "\n" + str(arr1))
-            text.insert(END, "\n\nAnswer:\n"+str(ans))
-
-        except ValueError:
-            text.delete(1.0, END)
-            tm.showerror("Dimensional Error", "Check The Entered Values")
-
-    except ValueError:
-        text.delete(1.0, END)
-        tm.showerror("Value Error", "Enter Valid Input(Only Numbers & Decimals)")
+    text.delete(1.0, END)
+    text.insert(END, "Entered Matrix 1:\n" + str(arr) + "\n" + "Entered Matrix 2:" + "\n" + str(arr1))
+    text.insert(END, "\n\nAnswer:\n"+str(ans))
 
 
 def tran():
-    try:
-        r1 = int(er1.get())
-        c1 = int(ec1.get())
+    data.set("Matrix Transpose")
 
-        data.set("Matrix Transpose")
-        arr = []
-        arr = (e1.get())
-        arr = arr.split(" ")
-        k = 0
-        for i in arr:
-            arr[k] = float(i)
-            k += 1
+    arr, arr1 = get_data(0)
+    if arr is None or arr1 is None:
+        return
 
-        arr = np.array([arr])   # We can add dtype = int64 and more
+    # Array transpose
+    ans = np.transpose(arr)
 
-        try:
-            arr = np.reshape(arr, (r1, c1))
-            ans = np.transpose(arr)
-
-            text.delete(1.0, END)
-            text.insert(END, "Entered Matrix 1:\n" + str(arr))
-            text.insert(END, "\n\nAnswer:\n" + str(ans))
-
-        except ValueError:
-            text.delete(1.0, END)
-            tm.showerror("Dimensional Error", "Check The Entered Values")
-
-    except ValueError:
-        text.delete(1.0, END)
-        tm.showerror("Value Error", "Enter Valid Input(Only Numbers & Decimals)")
+    text.delete(1.0, END)
+    text.insert(END, "Entered Matrix 1:\n" + str(arr))
+    text.insert(END, "\n\nAnswer:\n" + str(ans))
 
 
 def det():
-    try:
-        r1 = int(er1.get())
-        c1 = int(ec1.get())
+    data.set("Matrix Determinant")
 
-        data.set("Matrix Determinant")
-        arr = []
-        arr = (e1.get())
-        arr = arr.split(" ")
-        k = 0
-        for i in arr:
-            arr[k] = float(i)
-            k += 1
+    arr, arr1 = get_data(0)
+    if arr is None or arr1 is None:
+        return
 
-        arr = np.array([arr])  # We can add dtype = int64 and more
+    # Array determinant
+    ans = round(np.linalg.det(arr))
 
-        try:
-            arr = np.reshape(arr, (r1, c1))
-            ans = round(np.linalg.det(arr))
-
-            text.delete(1.0, END)
-            text.insert(END, "Entered Matrix 1:\n" + str(arr))
-            text.insert(END, "\n\nAnswer:\n" + str(ans))
-
-        except:
-            text.delete(1.0, END)
-            tm.showerror("Dimensional Error", "Check The Entered Values")
-
-    except ValueError:
-        text.delete(1.0, END)
-        tm.showerror("Value Error", "Enter Valid Input(Only Numbers & Decimals)")
+    text.delete(1.0, END)
+    text.insert(END, "Entered Matrix 1:\n" + str(arr))
+    text.insert(END, "\n\nAnswer:\n" + str(ans))
 
 
 def inv():
-    try:
-        r1 = int(er1.get())
-        c1 = int(ec1.get())
+    data.set("Matrix Inverse")
 
-        data.set("Matrix Inverse")
-        arr = []
-        arr = (e1.get())
-        arr = arr.split(" ")
-        k = 0
-        for i in arr:
-            arr[k] = float(i)
-            k += 1
+    arr, arr1 = get_data(0)
+    if arr is None or arr1 is None:
+        return
 
-        arr = np.array([arr])   # We can add dtype = int64 and more
+    # Array inverse
+    ans = np.linalg.inv(arr)
 
-        try:
-            arr = np.reshape(arr, (r1, c1))
-            ans = np.linalg.inv(arr)
+    text.delete(1.0, END)
+    text.insert(END, "Entered Matrix 1:\n" + str(arr))
+    text.insert(END, "\n\nAnswer:\n" + str(ans))
 
-            text.delete(1.0, END)
-            text.insert(END, "Entered Matrix 1:\n" + str(arr))
-            text.insert(END, "\n\nAnswer:\n" + str(ans))
 
-        except:
-            text.delete(1.0, END)
-            tm.showerror("Dimensional Error", "Check The Entered Values")
+def data_window(title, num):
+    global mat1_data, mat2_data
 
-    except:
-        text.delete(1.0, END)
-        tm.showerror("Value Error", "Enter Valid Input(Only Numbers & Decimals)")
+    # Data box
+
+    win = Toplevel()
+    win.title(title)
+    win.geometry("500x500")
+
+    def ok():
+        global mat1_data, mat2_data
+        # Assigns the entered values to the variables
+        if num == 1:
+            mat1_data = str(space.get(1.0, END)).strip()
+        elif num == 2:
+            mat2_data = str(space.get(1.0, END)).strip()
+
+        win.destroy()
+
+    space = Text(win, font="Verdana 12", wrap="word", width=1, undo=TRUE)
+    space.pack(expand=TRUE, fill=BOTH)
+    space.focus()
+
+    ok_btn = ttk.Button(win, text="OK", command=ok)
+    ok_btn.pack(side=BOTTOM)
+
+    if num == 1:
+        space.insert(1.0, mat1_data)
+    elif num == 2:
+        space.insert(1.0, mat2_data)
+
+
+def get_mat1():
+    global mat1_data
+
+    data_window("Matrix 1 Data: ", 1)
+
+
+def get_mat2():
+    global mat2_data
+
+    data_window("Matrix 2 Data: ", 2)
 
 
 def about_us():
@@ -316,14 +239,16 @@ def about_us():
 
 
 def helps():
+    # This will be executed when help is clicked which is in about
+
     text.delete(1.0, END)
-    text.insert(END, "1.Enter the matrix in linear form in the Enter Matrix 1 & 2 Values Section\n")
-    text.insert(END, "2.Press Space Bar After Each Value(Give No Space Before & After You Entering The Values)\n")
-    text.insert(END, "3.You can enter 'N' no. of values in the same Matrix 1 & 2 Values Section (OR) Box\n")
-    text.insert(END, "4.You can Scroll the Answer Box by using Scroll Wheel or UP & DOWN Arrows\n")
-    text.insert(END, "5.You can quickly write something in the Answer Box\n")
-    text.insert(END, "6.You can Select text in Answer Box by pressing CTRL+A(OR)Drag With Mouse(OR)Press SHIFT+ARROWS\n")
-    text.insert(END, "7.You can Copy the values by pressing CTRL+C keys\n")
+    text.insert(END, "1.Enter The Matrix By Clicking On Matrix 1 & 2 Data Buttons And Click OK To Save The Values\n")
+    text.insert(END, "2.Press Space Bar After Each Value And Press Enter Key After Each Row And Leave No Extra Spaces\n")
+    text.insert(END, "3.You Can Enter More Number Of Values By Maximizing The Data Window\n")
+    text.insert(END, "4.You Can Scroll The Answer Box By Using Scroll Wheel Or UP & DOWN Arrows\n")
+    text.insert(END, "5.You Can Quickly Write Something In The Answer Box\n")
+    text.insert(END, "6.You Can select text In Answer Box By Pressing CTRL+A(OR)Drag With Mouse(OR)Press SHIFT+ARROWS\n")
+    text.insert(END, "7.You Can Copy The Values By Pressing CTRL+C Keys\n")
 
     text.insert(END, "\n\n\n\t\t\t\tThanks for using\n")
 
@@ -340,6 +265,7 @@ am.add_command(label="Help", command=helps)
 
 
 # Top
+
 top = Frame(root)
 top.pack(expand=TRUE, fill=BOTH)
 
@@ -366,6 +292,7 @@ ec2 = ttk.Entry(top, width=60)
 ec2.grid(row=2, column=3, padx=3, pady=3)
 
 # Middle
+
 middle = Frame(root)
 middle.pack(expand=TRUE, fill=BOTH)
 
@@ -379,18 +306,21 @@ left.pack(side=LEFT)
 
 l1 = ttk.Label(left, text="Enter Matrix 1 Values : ")
 l1.pack(padx=3, pady=3)
-e1 = ttk.Entry(left, width=70)
-e1.pack(padx=6, pady=6)
+
+mat1 = ttk.Button(left, text="\t\t\tMatrix 1 Data\t\t\t\t", command=get_mat1)
+mat1.pack(padx=6, pady=6)
 
 right = Frame(middle)
 right.pack(side=RIGHT)
 
 l2 = ttk.Label(right, text="Enter Matrix 2 Values : ")
 l2.pack(padx=3, pady=3)
-e2 = ttk.Entry(right, width=70)
-e2.pack(padx=6, pady=6)
+
+mat1 = ttk.Button(right, text="\t\t\tMatrix 2 Data\t\t\t\t", command=get_mat2)
+mat1.pack(padx=6, pady=6)
 
 # Bottom
+
 top = Frame(root)
 top.pack(expand=TRUE, fill=BOTH)
 
@@ -410,6 +340,7 @@ b7 = ttk.Button(top, text="Inverse", command=inv)
 b7.pack(side=LEFT, expand=TRUE, fill=BOTH)
 
 # Answer
+
 answer = Frame(root)
 answer.pack(side=BOTTOM)
 

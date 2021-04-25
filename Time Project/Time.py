7aebd1a11daa13ef8fb516c6c8e93af0b804b393
Nikhil Gajam <65104684+nikhilgajam@users.noreply.github.com>
2021-04-25 16:58:59 +0530

    Time project updated

--- Time Project/Time.py
+++ Time Project/Time.py
@@ -1,7 +1,41 @@
+from tkinter import *
+import time
+
+root = Tk()
+root.title("Time")
+
+
+def update():
+    time_str = time.strftime("%I:%M:%S %p")
+    # To know about different directives
+    time_label['text'] = time_str
+
+    day_str = time.strftime("%A\n%B %d, %Y")
+    day_label['text'] = day_str
+
+    # After method is defined in root window and label classes and it takes time in milliseconds and function
+    root.after(1000, update)
+
+
+time_label = Label(root, font=('Bell MT', 60), fg='#dadce0', bg='#35363a')
+time_label.pack(fill=BOTH, expand=TRUE)
+
+day_label = Label(root, font=('Bell MT', 40))
+day_label.pack(fill=BOTH, expand=TRUE)
+
+update()
+
+root.mainloop()
+
+'''
+
+# TIME CODE WITH THREADING (SOMETIMES IT MAY CRASH WHEN YOU TRY TO CLOSE THE WINDOW)
+
 from datetime import datetime
 from tkinter import *
 import threading
 import time
+import sys
 
 root = Tk()
 root.title("Time")
@@ -11,30 +45,34 @@ l0.pack()
 l1 = Label(root, text="", font="Verdana 40")
 l1.pack()
 
+var = True
 
-def time_thing(i=0):
 
-    while TRUE:
+def time_thing():
+
+    while var:
         c = time.ctime(time.time())
         d = datetime.strptime(c[11:16], "%H:%M")
         d = d.strftime("%I:%M %p")
         d = d[:-3] + c[16:19] + d[5:]
 
-        if i > 0:
-            time.sleep(1)
-
         l1['text'] = d
         l0['text'] = " Day : " + c[:4] + "\n" + "Date  : " + c[8:11] + "\t" + " Year : " + c[20:] + "\n" + "Month: " + c[4:8]
 
-        i += 1
-
-
-def ok():
     root.destroy()
 
 
-t1 = threading.Thread(target=time_thing)
+def ok():
+    global var
+    var = False
+    sys.exit()
+
+t1 = threading.Thread(target=time_thing, args=())
 t1.start()
 
 root.protocol("WM_DELETE_WINDOW", ok)
 root.mainloop()
+
+
+'''
+

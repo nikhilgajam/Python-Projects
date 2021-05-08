5667645aebec7cd730b3ccd67d349080b31c4b33
Nikhil Gajam <65104684+nikhilgajam@users.noreply.github.com>
2021-05-08 18:53:26 +0530

    Search anything updated

--- Search Anything Project/Search Anything.py
+++ Search Anything Project/Search Anything.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import messagebox as tm
 from tkinter import filedialog
-from datetime import datetime
 from tkinter import ttk
 import webbrowser
 import wikipedia
+import threading
 import time
 
 root = Tk()
@@ -16,10 +16,10 @@ root.resizable(0, 0)
 # Commands
 
 
-def search(*args):
+def search():
 
     def save_as(event=""):
-        path = filedialog.asksaveasfilename(defaultextension=".txt", filetype=(("Text Document", "*.txt"), ("All Files", "*.*")))
+        path = filedialog.asksaveasfilename(initialfile="Search Anything.txt", defaultextension=".txt", filetype=(("Text Document", "*.txt"), ("All Files", "*.*")))
         with open(path, "w", encoding="utf8") as p:
             p.write(str(data))
 
@@ -46,17 +46,86 @@ def search(*args):
         text.config(cursor="exchange")
         e1.delete(0, END)
         e1.insert(0, random_name)
-        search()
+        new_thread()
 
     def about():
         webbrowser.open("https://youtube.com/nikhiltech")
         tm.showinfo("About Us", "This Code Is Written By Nikhil")
 
-    try:
+    sear = Toplevel()
+    sear.title("Data")
+    sear.geometry("752x457")
+    sear.config(cursor="watch")
 
-        dp.config(cursor="watch")
-        e1.config(cursor="watch")
-        b1.config(cursor="watch")
+    # Scrollbar
+
+    scroll = ttk.Scrollbar(sear)
+    scroll.pack(side=RIGHT, fill=Y)
+
+    # Text Box
+
+    text = Text(sear, wrap="word", yscrollcommand=scroll.set, fg="#e6e8eb", bg="#2c4079", font=20, undo=TRUE, spacing1=12, insertbackground="white")
+    text.bind("<Button-3>", popup)
+    text.bind("<Control-r>", randoms)
+    text.bind("<Control-s>", save_as)
+    text.focus_set()
+    text.pack(expand=TRUE, fill=BOTH, side=LEFT)
+    text.config(cursor="watch")
+
+    scroll.config(command=text.yview)
+
+    # Menu
+
+    mm = Menu(sear)
+    sear.config(menu=mm)
+
+    amm = Menu(mm, tearoff=0)
+    mm.add_cascade(label="Save", menu=amm)
+    amm.add_command(label="Save As            Ctrl+S", command=save_as)
+
+    emm = Menu(mm, tearoff=0)
+    mm.add_cascade(label="Edit", menu=emm)
+    emm.add_command(label="Cut               Ctrl+X", command=cut)
+    emm.add_command(label="Copy            Ctrl+C", command=copy)
+    emm.add_command(label="Paste            Ctrl+V", command=paste)
+    emm.add_separator()
+    emm.add_command(label="Undo            Ctrl+Z", command=text.edit_undo)
+    emm.add_command(label="Redo             Ctrl+Y", command=text.edit_redo)
+
+    rmm = Menu(mm, tearoff=0)
+    mm.add_cascade(label="Random", menu=rmm)
+    rmm.add_command(label="Open Random Suggestion             Ctrl+R", command=randoms)
+
+    asmm = Menu(mm, tearoff=0)
+    mm.add_cascade(label="About Us", menu=asmm)
+    asmm.add_command(label="About Creator", command=about)
+
+    # Popup Menu
+
+    popup_menu = Menu(root, tearoff=0)
+
+    popup_menu.add_command(label="Open Random Suggestion", command=randoms)
+    popup_menu.add_separator()
+    popup_menu.add_command(label="Save As", command=save_as)
+    popup_menu.add_separator()
+    popup_menu.add_command(label="Cut", command=cut)
+    popup_menu.add_command(label="Copy", command=copy)
+    popup_menu.add_command(label="Paste", command=paste)
+    popup_menu.add_separator()
+    popup_menu.add_command(label="Undo", command=text.edit_undo)
+    popup_menu.add_command(label="Redo", command=text.edit_redo)
+    popup_menu.add_separator()
+    popup_menu.add_command(label="Select All\t\t\t", command=select_all)
+
+    # Time And Date
+
+    time_date = time.strftime("%I:%M:%S %p  %A - %d/%B/%Y")
+
+    dp.config(cursor="watch")
+    e1.config(cursor="watch")
+    b1.config(cursor="watch")
+
+    try:
 
         ask = e1.get()
 
@@ -67,83 +136,14 @@ def search(*args):
         else:
             data = wikipedia.page(ask).content
 
-        sear = Toplevel()
-        sear.title("Data")
-        sear.geometry("752x457")
-
-        # Scrollbar
-
-        scroll = ttk.Scrollbar(sear)
-        scroll.pack(side=RIGHT, fill=Y)
-
-        # Text Box
-
-        text = Text(sear, wrap="word", yscrollcommand=scroll.set, fg="#e6e8eb", bg="#2c4079", font=20, undo=TRUE, spacing1=12, insertbackground="white")
-        text.bind("<Button-3>", popup)
-        text.bind("<Control-r>", randoms)
-        text.bind("<Control-s>", save_as)
-        text.focus_set()
-        text.pack(expand=TRUE, fill=BOTH, side=LEFT)
-
-        scroll.config(command=text.yview)
-
-        # Menu
-
-        mm = Menu(sear)
-        sear.config(menu=mm)
-
-        amm = Menu(mm, tearoff=0)
-        mm.add_cascade(label="Save", menu=amm)
-        amm.add_command(label="Save As            Ctrl+S", command=save_as)
-
-        emm = Menu(mm, tearoff=0)
-        mm.add_cascade(label="Edit", menu=emm)
-        emm.add_command(label="Cut               Ctrl+X", command=cut)
-        emm.add_command(label="Copy            Ctrl+C", command=copy)
-        emm.add_command(label="Paste            Ctrl+V", command=paste)
-        emm.add_separator()
-        emm.add_command(label="Undo            Ctrl+Z", command=text.edit_undo)
-        emm.add_command(label="Redo             Ctrl+Y", command=text.edit_redo)
-
-        rmm = Menu(mm, tearoff=0)
-        mm.add_cascade(label="Random", menu=rmm)
-        rmm.add_command(label="Open Random Suggestion             Ctrl+R", command=randoms)
-
-        asmm = Menu(mm, tearoff=0)
-        mm.add_cascade(label="About Us", menu=asmm)
-        asmm.add_command(label="About Creator", command=about)
-
-        # Popup Menu
-
-        popup_menu = Menu(root, tearoff=0)
-
-        popup_menu.add_command(label="Open Random Suggestion", command=randoms)
-        popup_menu.add_separator()
-        popup_menu.add_command(label="Save As", command=save_as)
-        popup_menu.add_separator()
-        popup_menu.add_command(label="Cut", command=cut)
-        popup_menu.add_command(label="Copy", command=copy)
-        popup_menu.add_command(label="Paste", command=paste)
-        popup_menu.add_separator()
-        popup_menu.add_command(label="Undo", command=text.edit_undo)
-        popup_menu.add_command(label="Redo", command=text.edit_redo)
-        popup_menu.add_separator()
-        popup_menu.add_command(label="Select All\t\t\t", command=select_all)
-
-        # Time And Date
-
-        c = time.ctime(time.time())
-        d = datetime.strptime(c[11:16], "%H:%M")
-        d = d.strftime("%I:%M %p")
-        d = d[:-3] + c[16:19] + d[5:]
-        time_date = d + "  " + c[:4] + "- " + c[8:10] + "/" + c[4:7] + "/" + c[20:] + " "
-
         # Random Wikipedia Suggestion
 
         if ask == "Type Anything Here":
             random_name = "Nikhil Tech"
         else:
             random_name = wikipedia.random()
+
+        sear.deiconify()
 
         # Data insertion
 
@@ -161,19 +161,25 @@ def search(*args):
         else:
             text.insert(END, ">>>  " + wikipedia.page(ask).title + "  <<<\n\n" + data)
 
+        sear.config(cursor="arrow")
+        text.config(cursor="arrow")
         root.config(cursor="arrow")
         dp.config(cursor="arrow")
         e1.config(cursor="arrow")
         b1.config(cursor="arrow")
 
-        sear.mainloop()
-
     except:
+        sear.destroy()
         tm.showerror("Problem", "Check Your Keyword And Internet Connection")
         root.config(cursor="arrow")
         dp.config(cursor="arrow")
         e1.config(cursor="arrow")
         b1.config(cursor="arrow")
+
+
+def new_thread(event=""):
+    x = threading.Thread(target=search, args=())
+    x.start()
 
 
 def about_us():
@@ -206,13 +212,13 @@ lbl.place(x=410, y=3)
 
 e1 = Entry(root, width=40, font="Verdana 19", fg="#e6e8eb", bg="#2c4079", bd=0, insertbackground="#e6e8eb")
 e1.insert(0, "Type Anything Here")
-e1.bind("<Return>", search)
+e1.bind("<Return>", new_thread)
 e1.focus_set()
 e1.place(x=141, y=200)
 
 # Button
 
-b1 = Button(root, text="Search", fg="#e6e8eb", bg="#2c4079", command=search)
+b1 = Button(root, text="Search", fg="#e6e8eb", bg="#2c4079", command=new_thread)
 b1.place(x=451, y=250)
 
 root.mainloop()
